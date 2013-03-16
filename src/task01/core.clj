a1f88f179af823160f98a39b67295daeb6784197
Andry Konchin <andry.konchin@gmail.com>
2013-03-16 19:43:19 +0200

    Добавил решение задачи

--- src/task01/core.clj
+++ src/task01/core.clj
@@ -1,9 +1,3 @@
-(ns task01.core
-  (:require [pl.danieljanus.tagsoup :refer :all])
-  (:gen-class))
-
-
-(defn get-links []
 " 1) Find all elements containing {:class \"r\"}.
 
 Example:
@@ -18,12 +12,31 @@ The link from the example above is 'https://github.com/clojure/clojure'.
 
   3) Return vector of all 10 links.
 
-Example: ['https://github.com/clojure/clojure', 'http://clojure.com/', . . .]
-"
+Example: ['https://github.com/clojure/clojure', 'http://clojure.com/', . . .]"
+
+(ns task01.core
+  (:require [pl.danieljanus.tagsoup :refer :all])
+  (:gen-class))
+
+(defn node-name [node] (get node 0))
+(defn node-attrs [node] (get node 1))
+(defn node-children [node] (subvec node 2))
+(defn node-nontext-children [node] (filter vector? (node-children node)))
+(defn link-href [link] (get (node-attrs link) :href))
+
+(defn tags [node]
+  (concat [node] (reduce concat (map tags (node-nontext-children node)))))
+
+(defn link? [n] (= (node-name n) :a))
+
+(defn google-result? [link] (= (get (node-attrs link) :class) "l"))
+
+(defn links [node]
+  (map link-href (filter google-result? (filter link? (tags node)))))
+
+(defn get-links []
   (let [data (parse "clojure_google.html")]
-    nil))
+    (links data)))
 
 (defn -main []
   (println (str "Found " (count (get-links)) " links!")))
-
-
