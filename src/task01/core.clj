efdbbc4f5a55a13a13eaf6f6ba6112e9de301609
Andry Konchin <andry.konchin@gmail.com>
2013-03-21 20:29:21 +0200

    Упростил решение.

--- src/task01/core.clj
+++ src/task01/core.clj
@@ -1,38 +1,17 @@
-" 1) Find all elements containing {:class \"r\"}.
-
-Example:
-[:h3 {:class \"r\"} [:a {:shape \"rect\", :class \"l\",
-                         :href \"https://github.com/clojure/clojure\",
-                         :onmousedown \"return rwt(this,'','','','4','AFQjCNFlSngH8Q4cB8TMqb710dD6ZkDSJg','','0CFYQFjAD','','',event)\"}
-                     [:em {} \"clojure\"] \"/\" [:em {} \"clojure\"] \" · GitHub\"]]
-
-   2) Extract href from the element :a.
-
-The link from the example above is 'https://github.com/clojure/clojure'.
-
-  3) Return vector of all 10 links.
-
-Example: ['https://github.com/clojure/clojure', 'http://clojure.com/', . . .]"
-
 (ns task01.core
   (:require [pl.danieljanus.tagsoup :refer :all])
   (:gen-class))
 
-(defn node-name [node] (get node 0))
-(defn node-attrs [node] (get node 1))
-(defn node-children [node] (subvec node 2))
-(defn node-nontext-children [node] (filter vector? (node-children node)))
-(defn link-href [link] (get (node-attrs link) :href))
+(defn link-href [link] ((attributes link) :href))
+(defn google-result? [link] (= ((attributes link) :class) "l"))
 
-(defn tags [node]
-  (cons node (reduce concat (map tags (node-nontext-children node)))))
-
-(defn link? [n] (= (node-name n) :a))
-
-(defn google-result? [link] (= (get (node-attrs link) :class) "l"))
+(defn select-links [node]
+  (if (= (tag node) :a)
+    [node]
+    (reduce concat (map select-links (filter vector? (children node))))))
 
 (defn links [node]
-  (map link-href (filter google-result? (filter link? (tags node)))))
+  (map link-href (filter google-result? (select-links node))))
 
 (defn get-links []
   (let [data (parse "clojure_google.html")]
@@ -40,3 +19,6 @@ Example: ['https://github.com/clojure/clojure', 'http://clojure.com/', . . .]"
 
 (defn -main []
   (println (str "Found " (count (get-links)) " links!")))
+
+(links (parse "clojure_google.html"))
+
